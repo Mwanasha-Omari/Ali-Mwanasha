e98e3de1b753d832447230311bc7aa6ebd03b457
Mwanasha-Omari <alimwanashaomari@gmail.com>
2024-02-07 08:22:28 +0300

    add one file

---
+++ Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+        var name = "ali Mwanasha"
+        println (name)
+        var age = 19
+        println (age)
+        var number = +254705210849
+        println(number)
+        var weight = 53
+        println(weight)
+        var nationality = "kenyan"
+        println(nationality)
+        val citizen = true
+        val notcitizen = false
+        println(citizen)
+
+
+
+
+
+
+
+
+}
